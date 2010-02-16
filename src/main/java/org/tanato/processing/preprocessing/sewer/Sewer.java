18f337c800c199dfbeae3dde7142d4a099c24e20
ebocher <ebocher@irstv>
2010-02-16 16:49:18 +0000

    FEATURE data for tanato sewer snapping

--- src/main/java/org/tanato/processing/preprocessing/sewer/Sewer.java
+++ src/main/java/org/tanato/processing/preprocessing/sewer/Sewer.java
@@ -1,3 +1,4 @@
+
 package org.tanato.processing.preprocessing.sewer;
 
 import java.io.File;
@@ -7,7 +8,6 @@ import org.gdms.data.DataSource;
 import org.gdms.data.DataSourceCreationException;
 import org.gdms.data.DataSourceFactory;
 import org.gdms.data.SpatialDataSourceDecorator;
-
 import org.gdms.driver.DriverException;
 import org.gdms.driver.driverManager.DriverLoadException;
 
@@ -17,75 +17,77 @@ import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.Point;
 
-
 public class Sewer {
 
 	public static DataSourceFactory dsf = new DataSourceFactory();
 	public static GeometryFactory gf = new GeometryFactory();
-	public static String path1 = "data/small_courbes.shp";
-	public static String path2 = "data/small_courbes.shp";
+	public static String building = "data/modelisation/chezine/bati_extrait.shp";
+	public static String sewer = "data/modelisation/chezine/sewer.shp";
 
-	public static void main(String[] args) throws DriverLoadException, DataSourceCreationException, DriverException {
-		//ArrayList<Geometry> sewer =getSewer();
+	public static void main(String[] args) throws DriverLoadException,
+			DataSourceCreationException, DriverException {
+		// ArrayList<Geometry> sewer =getSewer();
 	}
-	
-	public static ArrayList<Geometry> getSewer() throws DriverLoadException, DataSourceCreationException, DriverException {
+
+	public static ArrayList<Geometry> getSewer() throws DriverLoadException,
+			DataSourceCreationException, DriverException {
 		ArrayList<Geometry> result = new ArrayList<Geometry>();
 		ArrayList<Geometry> resultok = new ArrayList<Geometry>();
 		ArrayList<Geometry> resultnotok = new ArrayList<Geometry>();
-		
-		DataSource mydata1 = dsf.getDataSource(new File(path1));
-		DataSource mydata2 = dsf.getDataSource(new File(path2));
-		SpatialDataSourceDecorator sds1 = new SpatialDataSourceDecorator(mydata1);
-		SpatialDataSourceDecorator sds2 = new SpatialDataSourceDecorator(mydata2);
-		
-		//creation of the tree
+
+		DataSource mydata1 = dsf.getDataSource(new File(building));
+		DataSource mydata2 = dsf.getDataSource(new File(sewer));
+		SpatialDataSourceDecorator sds1 = new SpatialDataSourceDecorator(
+				mydata1);
+		SpatialDataSourceDecorator sds2 = new SpatialDataSourceDecorator(
+				mydata2);
+
+		// creation of the tree
 		sds1.open();
 		ArrayList<Geometry> geoms = new ArrayList<Geometry>();
-        		for (int i = 0; i < sds1.getRowCount(); i++) {
+		for (int i = 0; i < sds1.getRowCount(); i++) {
 			Geometry geom1 = sds1.getGeometry(i);
-			geoms.add( geom1);
+			geoms.add(geom1);
 		}
-        KDTree3D tree = new KDTree3D(geoms);
+		KDTree3D tree = new KDTree3D(geoms);
 		sds1.close();
-		
-		//creation of the links
+
+		// creation of the links
 		sds2.open();
 		for (int j = 0; j < sds2.getRowCount(); j++) {
-				Geometry geom2 = sds2.getGeometry(j);
-				LineString newls;
-				if (geom2.getClass().getName()=="Polygon")
-				{	Coordinate[] coord = new Coordinate[2];
-					coord[0]=((Point)geom2).getCoordinate();
-					coord[1]=(tree.nearestNeighborWithInfZ((Point)geom2)).getCoordinate();
-					newls=gf.createLineString(coord);
-					result.add(newls);	
-				}
+			Geometry geom2 = sds2.getGeometry(j);
+			LineString newls;
+			if (geom2.getClass().getName() == "Polygon") {
+				Coordinate[] coord = new Coordinate[2];
+				coord[0] = ((Point) geom2).getCoordinate();
+				coord[1] = (tree.nearestNeighborWithInfZ((Point) geom2))
+						.getCoordinate();
+				newls = gf.createLineString(coord);
+				result.add(newls);
+			}
 		}
-		
-		//research of the cases with some houses in the way
-		for (int i=0;i<result.size();i++)
-		{
+
+		// research of the cases with some houses in the way
+		for (int i = 0; i < result.size(); i++) {
 			for (int j = 0; j < sds2.getRowCount(); j++) {
 				Geometry geom2 = sds2.getGeometry(j);
 				LineString newls;
 				boolean b;
-				if (geom2.getClass().getName()=="Polygon")
-					{	Coordinate[] coord = new Coordinate[2];
-						coord[0]=((Point)geom2).getCoordinate();
-						coord[1]=(tree.nearestNeighborWithInfZ((Point)geom2)).getCoordinate();
-						newls=gf.createLineString(coord);
-						result.add(newls);	
-					}
+				if (geom2.getClass().getName() == "Polygon") {
+					Coordinate[] coord = new Coordinate[2];
+					coord[0] = ((Point) geom2).getCoordinate();
+					coord[1] = (tree.nearestNeighborWithInfZ((Point) geom2))
+							.getCoordinate();
+					newls = gf.createLineString(coord);
+					result.add(newls);
+				}
 			}
 		}
-		
-		//resolving the cases with some houses in the way
-		while (resultnotok.size()>0)
-			{
-			}
-			
-		
+
+		// resolving the cases with some houses in the way
+		while (resultnotok.size() > 0) {
+		}
+
 		sds2.close();
 		return result;
 	}
