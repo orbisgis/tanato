a0b9b5f8698c82268c6d6f858ced60e429bb9ffb
adelin <adelin@irstv>
2010-06-11 16:08:19 +0000

    FEATURE adding test ConstrainedDelaunayLevelEdge.

--- src/test/java/org/tanato/triangulation/ConstrainedDelaunayLevelEdge.java
+++ src/test/java/org/tanato/triangulation/ConstrainedDelaunayLevelEdge.java
@@ -1,6 +1,7 @@
 package org.tanato.triangulation;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import org.gdms.data.DataSource;
@@ -12,27 +13,30 @@ import org.gdms.driver.driverManager.DriverLoadException;
 import org.grap.utilities.EnvelopeUtil;
 import org.jdelaunay.delaunay.DelaunayError;
 import org.jdelaunay.delaunay.MyDrawing;
+import org.jdelaunay.delaunay.MyEdge;
 import org.jdelaunay.delaunay.MyMesh;
 import org.jdelaunay.delaunay.MyPoint;
-import org.jdelaunay.delaunay.MyPolygon;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.io.ParseException;
 
-public class ConstrainedDelaunayPolygon {
+public class ConstrainedDelaunayLevelEdge {
 
 	public static DataSourceFactory dsf = new DataSourceFactory();
 
-	public static String path = "src/test/resources/data/source/polygon/polygon2d.shp";
+	public static String path = "src/test/resources/data/source/chezine/courbes_niveaux.shp";
 
 	public static void main(String[] args) throws DriverLoadException,
 			DataSourceCreationException, DriverException, DelaunayError,
-			ParseException {
+			ParseException, IOException {
 
+		long start = System.currentTimeMillis();
+		
 		MyMesh aMesh = new MyMesh();
+		aMesh.setVerbose(true);
 		DataSource mydata = dsf.getDataSource(new File(path));
 
 		SpatialDataSourceDecorator sds = new SpatialDataSourceDecorator(mydata);
@@ -47,21 +51,31 @@ public class ConstrainedDelaunayPolygon {
 		ArrayList<MyPoint> points = new ArrayList<MyPoint>();
 
 		for (int i = 0; i < coords.length - 1; i++) {
+			coords[i].z = 0;
 			points.add(new MyPoint(coords[i]));
-
 		}
 
-		aMesh.setPoints(points);
-
-		for (int i = 0; i < 1; i++) {
-
+		int z;
+		for (long i = 0; i < sds.getRowCount(); i++) {
 			Geometry geom = sds.getGeometry(i);
+
+			z = sds.getFieldValue(i, 2).getAsInt();
 
 			for (int j = 0; j < geom.getNumGeometries(); j++) {
 				Geometry subGeom = geom.getGeometryN(j);
 
-				if (subGeom instanceof Polygon) {
-					aMesh.addPolygon(new MyPolygon((Polygon) subGeom));
+				if (subGeom instanceof LineString) {
+
+					Coordinate c1 = subGeom.getCoordinates()[0];
+					Coordinate c2;
+					c1.z = z;
+					for (int k = 1; k < subGeom.getCoordinates().length; k++) {
+						c2 = subGeom.getCoordinates()[k];
+						c2.z = z;
+						aMesh.addLevelEdge(new MyEdge(new MyPoint(c1),
+								new MyPoint(c2)));
+						c1 = c2;
+					}
 				}
 			}
 		}
@@ -69,10 +83,24 @@ public class ConstrainedDelaunayPolygon {
 		sds.close();
 
 		aMesh.processDelaunay();
+		
+		System.out.println("\npoint : "+aMesh.getNbPoints()+"\nedges : "+aMesh.getNbEdges()+"\ntriangles : "+aMesh.getNbTriangles());
+		
+		
+//		aMesh.removeFlatTriangles();//FIXME too long!
 
+		
+		long end = System.currentTimeMillis();
+		System.out.println("Duration " + (end-start)+"ms soit "+((end-start)/60000)+"min");
+		
 		MyDrawing aff2 = new MyDrawing();
 		aff2.add(aMesh);
 		aMesh.setAffiche(aff2);
+//		aMesh.VRMLexport();
+
+		
+
+
 	}
 
 }
