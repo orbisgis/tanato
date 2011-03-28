803d7e65cb0b9ca43a11e6a27839bb0470569647
agueganno <a9emen@gmail.com>
2011-03-28 11:27:40 +0000

    Adding documentation, fixing an exception message.

--- src/main/java/org/tanato/factory/TINFeatureFactory.java
+++ src/main/java/org/tanato/factory/TINFeatureFactory.java
@@ -12,7 +12,7 @@ import org.jdelaunay.delaunay.DTriangle;
 import org.jdelaunay.delaunay.DelaunayError;
 
 /**
- *
+ * A small factory used to create jDelaunay objects from JTS geometries.
  * @author ebocher
  */
 public class TINFeatureFactory {
@@ -34,14 +34,26 @@ public class TINFeatureFactory {
 			new DEdge(coords[2].x, coords[2].y, coords[2].z, coords[0].x, coords[0].y, coords[0].z));
         }
 
+        /**
+         * A factory to create a DPoint from a Geometry
+         * @param geom
+         * @return
+         * @throws DelaunayError
+         */
         public static DPoint createDPoint(Geometry geom) throws DelaunayError{
                 Coordinate[] coords = geom.getCoordinates();
                 if (coords.length != 1) {
-                        throw new IllegalArgumentException("The geometry must be a triangle");
+                        throw new IllegalArgumentException("The geometry must be a point");
                 }
                 return new DPoint(coords[0]);
         }
 
+        /**
+         * A factory to create a DPoint from a Coordinate.
+         * @param geom
+         * @return
+         * @throws DelaunayError if one of the coordinate values is Double.Nan
+         */
         public static DPoint createDPoint(Coordinate coord) throws DelaunayError {
                 return new DPoint(coord);
         }
